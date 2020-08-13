5f89272fd1387ee0c66d18533a1d264697d98c85
Kamál F <kamal.fontirroig@gmail.com>
2020-08-13 16:44:26 -0400

    Agregados VIDBUTTONS para play/pause y mute/unmute, un poco de cambios al estilo de los titulos de las CARDS

--- assets/js/index_script.js
+++ assets/js/index_script.js
@@ -14,3 +14,53 @@
     showNextQuote();
 
 })()
+
+window.onload = function () {   
+    playPause();
+    muteUnmute();
+}
+
+
+
+function togglePlay() {
+    var myVideo = document.getElementById("header-video");
+    if (myVideo.paused) {
+        myVideo.play();
+        playPause();
+    }
+    else {
+        myVideo.pause();
+        playPause();
+    }
+    
+}
+
+
+function playPause() {
+    var ppbutton = document.getElementById("vidbuttonPlay");
+    var myVideo = document.getElementById("header-video");
+    if (myVideo.paused) {
+        ppbutton.innerHTML = `<i class="fa fa-pause"></i>`;
+    }
+    else {
+        ppbutton.innerHTML = `<i class="fa fa-play"></i>`;
+    }
+}
+
+
+function muteUnmute() {
+    var mubutton = document.getElementById("vidbuttonMute");
+    myVideo = document.getElementById("header-video");
+    if (myVideo.muted) {
+        mubutton.innerHTML = `<i class="fa fa-volume-up"></i>`;
+    }
+    else {
+        mubutton.innerHTML = `<i class="fa fa-volume-off"></i>`;
+    }
+}
+
+function toggleMute() {
+    var myVideo = document.getElementById("header-video");
+    myVideo.muted = !myVideo.muted;
+    muteUnmute();
+}
