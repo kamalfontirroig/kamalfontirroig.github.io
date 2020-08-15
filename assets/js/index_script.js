dac1709dbb48183b6f3c847f505c814c155aa59e
Kamál F <kamal.fontirroig@gmail.com>
2020-08-14 20:22:06 -0400

    Cambiado a una tematica espacial, con parallax. Falta mejorar y estilizar las CARDS, agregar links a los proyectos y mejorar responsividad del diseño

--- assets/js/index_script.js
+++ assets/js/index_script.js
@@ -3,12 +3,19 @@
     var quotes = $(".fading-quotes");
     var quoteIndex = -1;
 
+    var titulo = $(".titulo-div");
+    titulo.fadeIn(5000)
+
+    var nav = $(".navito");
+    nav.fadeIn(5000)
+
     function showNextQuote() {
         ++quoteIndex;
+        quotes.delay(4000)
         quotes.eq(quoteIndex % quotes.length)
-            .fadeIn(1000)
-            .delay(4000)
-            .fadeOut(3000, showNextQuote);
+            .fadeIn(5000)
+            .delay(10000)
+            .fadeOut(5000, showNextQuote);
     }
 
     showNextQuote();
@@ -17,13 +24,16 @@
 
 window.onload = function () {   
     playPause();
-    muteUnmute();
+    audioMute();
+    audio = document.getElementById("audio");
+    audio.volume = 0.5;
+   
 }
 
 
 
 function togglePlay() {
-    var myVideo = document.getElementById("header-video");
+    var myVideo = document.getElementById("videobg");
     if (myVideo.paused) {
         myVideo.play();
         playPause();
@@ -38,29 +48,47 @@ function togglePlay() {
 
 function playPause() {
     var ppbutton = document.getElementById("vidbuttonPlay");
-    var myVideo = document.getElementById("header-video");
+    var myVideo = document.getElementById("videobg");
     if (myVideo.paused) {
-        ppbutton.innerHTML = `<i class="fa fa-pause"></i>`;
+        ppbutton.innerHTML = `<i class="fa fa-pause" class="z-front"></i>`;
     }
     else {
-        ppbutton.innerHTML = `<i class="fa fa-play"></i>`;
+        ppbutton.innerHTML = `<i class="fa fa-play"class="z-front"></i>`;
     }
 }
 
 
 function muteUnmute() {
     var mubutton = document.getElementById("vidbuttonMute");
-    myVideo = document.getElementById("header-video");
+    myVideo = document.getElementById("videobg");
     if (myVideo.muted) {
-        mubutton.innerHTML = `<i class="fa fa-volume-up"></i>`;
+        mubutton.innerHTML = `<i class="fa fa-volume-up" class="z-front"></i>`;
     }
     else {
-        mubutton.innerHTML = `<i class="fa fa-volume-off"></i>`;
+        mubutton.innerHTML = `<i class="fa fa-volume-off" class="z-front"></i>`;
     }
 }
 
 function toggleMute() {
-    var myVideo = document.getElementById("header-video");
+    var myVideo = document.getElementById("videobg");
     myVideo.muted = !myVideo.muted;
     muteUnmute();
+}
+
+
+function audioMute() {
+    var mubutton = document.getElementById("vidbuttonMute");
+    audio = document.getElementById("audio");
+    if (audio.muted) {
+        mubutton.innerHTML = `<i class="fa fa-volume-up" class="z-front"></i>`;
+    }
+    else {
+        mubutton.innerHTML = `<i class="fa fa-volume-off" class="z-front"></i>`;
+    }
+}
+
+function toggleAudio() {
+    var audio = document.getElementById("audio");
+    audio.muted = !audio.muted;
+    audioMute();
 }
