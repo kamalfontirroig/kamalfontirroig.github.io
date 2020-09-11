0654dd37fffd66c17cf952d738d292891ae6b1cb
Kamál F <kamal.fontirroig@gmail.com>
2020-09-11 04:51:11 -0300

    actualizado el CARRITO, version final para el desafio tecnico

--- carrito/js/carrito.js
+++ carrito/js/carrito.js
@@ -1,10 +1,25 @@
 var productosEnCarro = []
+var carroAbierto = false;
 var formatter = new Intl.NumberFormat('es-CL', {
     style: 'currency',
     currency: 'CLP',
 })
 
 
+function toggleCarro() {
+    if (productosEnCarro.length < 1){
+        alertify.message('El carro está vacío', 2)
+    } else {
+    if (carroAbierto) {
+        carroAbierto = false
+        $("#contenedor-carro").toggleClass("d-none")
+    } else {
+        carroAbierto = true
+        $("#contenedor-carro").toggleClass("d-none")
+       
+    }
+}
+}
 const agregarAlCarro = (idProducto) => {
 
     var productoSeleccionado = listaProductos.filter(function (producto) {
@@ -15,28 +30,35 @@ const agregarAlCarro = (idProducto) => {
         productosEnCarro.map((producto) => {
             if (producto.id == productoSeleccionado.id) {
                 producto.cantidad += 1
+                if (!carroAbierto){
+                alertify.success('Agregado ' + productoSeleccionado.nombre + ' al carro (Cantidad: ' + producto.cantidad + ')', 4);
+                }
             }
         })
     } else {
         productoSeleccionado.cantidad = 1
         productosEnCarro.push(productoSeleccionado)
+        alertify.success('Agregado ' + productoSeleccionado.nombre + ' al carro', 4);
     }
-
+    
     renderCarro()
     actualizarCantidad()
 }
 
 
 const eliminarDelCarro = (idProducto) => {
-
+    var productoEliminado =  productosEnCarro.filter(function (producto) {
+        return producto.id == idProducto;
+    })[0]
     var productosMantenidos = productosEnCarro.filter(function (producto) {
         return producto.id != idProducto;
     })
-
+   
     productosEnCarro = productosMantenidos
     console.log(productosEnCarro)
     renderCarro()
     actualizarCantidad()
+    alertify.notify(`Eliminado ${productoEliminado.nombre} `, 'error', 5);
 }
 
 
@@ -44,38 +66,48 @@ function renderCarro() {
 
     $("#carro").empty()
     if ( productosEnCarro.length > 0) {
+        
+        $("#carrito-icono").html('<img src="img/carrito-lleno.PNG" height="50px" >')
     var productosEnCarroHTML = '<h5 class="card-title"> Carro de Compras</h5>' +
-                              '<table class="table">'
+                              '<table class="table">' +
+                              '<td> Cantidad </td> <td> Producto </td> <td>  </td>'
 
     for (i = 0; i < productosEnCarro.length; i++) {
         var producto =
-                    '<tr>' +
-                       '<td>' +
-                            '<input class="input-cantidad d-inline"  id="cantidadDeProducto'+productosEnCarro[i].id+'" min="0" type="number" onchange="actualizarCantidad()" value=' + productosEnCarro[i].cantidad + '>' + 
-                        '</td>' +
-                        '<td>' +
-                            'x <b>' + productosEnCarro[i].nombre + '</b>' +
-                            '<p> ' + formatter.format(productosEnCarro[i].precioUnitario) + '</p>' +
-                        '</td>' +
-                        '<td>' +
-                            '<div class="btn btn-danger d-inline eliminar" onclick="eliminarDelCarro(' + productosEnCarro[i].id + ')" >X' +
-                            '</div>' +
-                        '</td>' + 
-                    '</tr>' 
+                    `<tr>
+                       <td> 
+                            <input class="input-cantidad d-inline"  id="cantidadDeProducto${productosEnCarro[i].id}" min="0" type="number" onchange="actualizarCantidad()" value=${productosEnCarro[i].cantidad}>  
+                        </td> 
+                        <td>
+                        x <b>${productosEnCarro[i].nombre}</b>  ${formatter.format(productosEnCarro[i].precioUnitario)}
+                        </td> 
+                        <td> 
+                            <div class="btn btn-primary d-inline botones-carro boton-falso" onclick="aumentarCantidad(${productosEnCarro[i].id})"><b>+</b>
+                            </div> 
+                            <div class="btn btn-secondary d-inline botones-carro boton-falso disminuir" onclick="disminuirCantidad(${productosEnCarro[i].id})"><b>-</b>
+                            </div> 
+                            <div class="btn btn-danger d-inline botones-carro boton-falso" onclick="eliminarDelCarro(${productosEnCarro[i].id})" ><b>X</b> 
+                            </div> 
+                        </td>  
+                    </tr> `
          
         productosEnCarroHTML += producto
     }
 
     productosEnCarroHTML = productosEnCarroHTML + '</table>'
-    }   
+    } else {
+        $("#carrito-icono").html('<img src="img/carrito.PNG" height="50px" >')
+    }
     $("#carro").append(productosEnCarroHTML)
-
+  
 }
+
 
 
 function actualizarCantidad() {
 
     $("#precio-total").empty()
+    var cantidadProductosEnCarro = 0;
     var precioTotal = 0
 
     for (i = 0; i < productosEnCarro.length; i++) {
@@ -84,16 +116,49 @@ function actualizarCantidad() {
         if (productosEnCarro[i].cantidad == 0) {
             eliminarDelCarro(productosEnCarro[i].id)
         } else if (productosEnCarro[i].cantidad > 0) {
+            cantidadProductosEnCarro += productosEnCarro[i].cantidad
             precioTotal += productosEnCarro[i].precioUnitario * productosEnCarro[i].cantidad
         } else {
             $("#cantidadDeProducto" + productosEnCarro[i].id).val("1")
             productosEnCarro[i].cantidad = parseInt($("#cantidadDeProducto" + productosEnCarro[i].id).val())
+            cantidadProductosEnCarro += productosEnCarro[i].cantidad
             precioTotal += productosEnCarro[i].precioUnitario * productosEnCarro[i].cantidad
         }
 
     }
 
    if (precioTotal != 0) {
-       $("#precio-total").html('<h5 class="card-title my-2 mx-3 d-inline">Total ' + formatter.format(precioTotal) + '</h5>' + '<div class="btn btn-success" onclick="alert(`a pagar!`)">Pagar</div>')
-   }
+       $("#precio-total").html(`<div class="container-fluid text-right"><h5 class="card-title d-inline">Total  ${formatter.format(precioTotal)} </h5> <div class="btn btn-success boton-falso" onclick="alert('Realicé esta página con dedicación para respaldar lo que dije: estoy sumamente entusiasmado de seguir aprendiendo y hacer lo mejor que pueda. Sé que todo el empeño que pongo en mi trabajo no solo se plasmará en un buen producto, también, quedará en mí como conocimiento y experiencia. Saludos!')">Pagar</div><div>`)
+       $("#cantidad-en-carro").addClass("d-inline")
+       $("#cantidad-en-carro").html(cantidadProductosEnCarro)
+    } else {
+       $("#cantidad-en-carro").addClass("d-none")
+    }
+}
+
+
+function aumentarCantidad(idProducto){
+    productosEnCarro.map(producto => { 
+                            if (producto.id == idProducto) {
+                                producto.cantidad++
+                            $("#cantidadDeProducto" + producto.id).val(producto.cantidad)
+                                actualizarCantidad()            
+                            }   
+    })
+    
+}
+
+function disminuirCantidad(idProducto){
+    productosEnCarro.map(producto => { 
+                            if (producto.id == idProducto && producto.cantidad > 1) {
+                                producto.cantidad--
+                            $("#cantidadDeProducto" + producto.id).val(producto.cantidad)
+                                actualizarCantidad()            
+                            } else {
+                                if (producto.id == idProducto) {
+                                alertify.notify('Aprete X para eliminar', 'error', 2);
+                                }
+                            }
+    })
+    
 }
--- carrito/js/vista.js
+++ carrito/js/vista.js
@@ -5,31 +5,67 @@ var formatter = new Intl.NumberFormat('es-CL', {
 
 
 window.onload = function () {
+    renderProductos(listaProductos)
+    $("body").addClass("d-inline")
+}
 
-    var cartasDeProductos = ''
+function renderProductos(productos) {
 
-    for (i = 0; i < listaProductos.length; i++) {
+    $("#productos").empty()
+    var cartasDeProductos = `<div class="flex-productos">`
+
+    for (i = 0; i < productos.length; i++) {
         var producto =
-                    '<div class="card producto mx-1 my-1">' +
-                        '<div class="card-body">' +
-                            '<h5 class="card-title">' + listaProductos[i].nombre + '</h5>' +
-                            '<img class="card-img-top" src="img/' + listaProductos[i].img + '" >' +
-                            '<p class="card-text">' + listaProductos[i].desc + '</p>' +
-                            '<div class="card-footer">' +
-                                '<div class="d-inline precio-unitario" ><b>' + formatter.format(listaProductos[i].precioUnitario) +'</b>' +
-                                '</div>' +
-                                '<div id="producto" class="btn btn-primary d-inline"' + listaProductos[i].id + '" onclick="agregarAlCarro(' + listaProductos[i].id + ')" >Agregar' + 
-                                '</div>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>'
+                   `<div class="flex-producto">
+                    <div class="card producto mx-1 my-1">
+                        <div class="card-body">
+                            <h5 class="card-title">${productos[i].nombre}</h5>
+                            <img class="card-img-top" src="img/${productos[i].img}" >
+                            <p class="card-text">${productos[i].desc} </p>
+                            <div class="card-footer">
+                                <div class="d-inline precio-unitario" ><b> ${formatter.format(productos[i].precioUnitario)}</b>
+                                </div>
+                                <div id="producto" class="btn btn-primary d-inline boton-falso" onclick="agregarAlCarro(${productos[i].id})" >Agregar
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                    </div>`
 
         cartasDeProductos += producto
     }
+    cartasDeProductos +=`</div>`
 
-    $("#productos").append('<div class="row">' + cartasDeProductos + '</div>')
+    $("#productos").append(cartasDeProductos)
 }
 
+function buscarProducto(fraseBuscada) {
+var palabrasBuscadas = fraseBuscada.split(" ")
+var productosEncontrados = []
+console.log()
+if (palabrasBuscadas.length > 0){
+    console.log()
+    for (i=0; i < listaProductos.length; i++){
+        productoConcatenado = listaProductos[i].nombre + listaProductos[i].desc
+        productoConcatenado = productoConcatenado.toLowerCase()
+        for (j=0; j < palabrasBuscadas.length; j++){
+            if (productoConcatenado.indexOf(palabrasBuscadas[j].toLowerCase()) >= 0) {
+                productosEncontrados.push(listaProductos[i])   
+            }
+        }
+    }
+    console.log()
+    if (productosEncontrados.length > 0){
+        renderProductos(productosEncontrados)
+    } else {
+        console.log()
+        alertify.error("No se encontraron productos", 5)
+        renderProductos(listaProductos)
+    }
 
-
+    
+} else {
+    renderProductos(listaProductos)
+}
+}
 
